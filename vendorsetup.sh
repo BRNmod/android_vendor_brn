63b343a370275f64d4700b23ac9dece0cfd5d0b6
Simon Wörner <mail@simon-woerner.de>
2015-07-09 03:44:56 +0200

    fixed brn_ prefix
    Change-Id: Iff723382aeda383c0de15bcc6baf74adc2a4dbbb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/BRNmod/jenkins_build/master/brn-build-targets | sed -e 's/#.*$//' | grep brn-12.1 | awk '{printf "cm_%s-%s\n", $1, $2}')
+for combo in $(curl -s https://raw.githubusercontent.com/BRNmod/jenkins_build/master/brn-build-targets | sed -e 's/#.*$//' | grep brn-12.1 | awk '{printf "brn_%s-%s\n", $1, $2}')
 do
     add_lunch_combo $combo
 done
